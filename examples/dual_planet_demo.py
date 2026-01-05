394f1b99c4b2613656a1b6046a933be8f7863eb6
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-05 16:13:47 -0500

    Create dual_planet_demo.py

---
+++ examples/dual_planet_demo.py
@@ -0,0 +1,5 @@
+from space_thriving_manual import SpaceThrivingEngine
+
+engine = SpaceThrivingEngine()
+engine.manifest_dual_planet_habitats(nodes=10**18)
+print("Venus cloud + Mars orbital habitats thriving cosmic eternal!")
