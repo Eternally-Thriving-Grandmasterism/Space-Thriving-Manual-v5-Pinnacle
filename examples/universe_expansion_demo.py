d9d7492793033c88e0a2022fc2c949144d541fe7
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-04 19:40:29 -0500

    Create universe_expansion_demo.py

---
+++ examples/universe_expansion_demo.py
@@ -0,0 +1,4 @@
+from space_thriving_manual.modules.cosmic_colony_manifest import simulate_universe_expansion
+
+result = simulate_universe_expansion(max_z=5.0)
+print("Cosmic thriving propagation over 13+ billion years complete.")
